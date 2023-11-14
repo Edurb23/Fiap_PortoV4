c75c54141cf97049ce711b194a73ce5af2b8e7b4
Edurb23 <rm551987@fiap.com.br>
2023-11-13 22:06:51 -0300

    ultima atualização

--- src/app/cadastro/page.jsx
+++ src/app/cadastro/page.jsx
@@ -99,7 +99,7 @@ export default function Cadastro() {
             </div>
             <div className='link'>
                 <Link href="/homeadmin" className='link'>Voltar</Link>
-                <button>Cadastrar</button>
+                <Link href="/homeadmin" className='link'><button>Cadastrar</button></Link>
             </div>
         </form>
     </div>  
